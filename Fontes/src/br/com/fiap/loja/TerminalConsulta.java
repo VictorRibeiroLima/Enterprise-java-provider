36c80841805136f8caf9c7f2308914b6adbb6556
VictorRibeiroLima <38430966+VictorRibeiroLima@users.noreply.github.com>
2019-03-13 09:05:47 -0300

    Add files via upload

---
+++ Fontes/src/br/com/fiap/loja/TerminalConsulta.java
@@ -0,0 +1,32 @@
+package br.com.fiap.loja;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import org.apache.axis2.AxisFault;
+
+import br.com.fiap.loja.bo.EstoqueBO;
+import br.com.fiap.loja.singleton.PropertySingleton;
+import br.com.fiap.loja.to.ProdutoTO;
+
+public class TerminalConsulta {
+
+	public static void main(String[] args) {
+		String nome = PropertySingleton.getInstance().getProperty("nome");
+		LocalDate hoje = LocalDate.now();
+		DateTimeFormatter formatador = 	DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(nome + " " + hoje.format(formatador));
+		System.out.println("Digite o código do produto desejado: ");
+		Scanner scanner = new Scanner(System.in);
+		int codigo = scanner.nextInt();
+		scanner.close();
+		ProdutoTO produto;
+		try {
+			produto = EstoqueBO.consultarProduto(codigo);
+			System.out.println(produto.getDescricao());
+		} catch (AxisFault e) {
+			e.printStackTrace();
+		}		
+	}
+}
